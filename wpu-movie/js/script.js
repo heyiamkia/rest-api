2e88a714b5e022f898a0d832e229ce30f6c548c2
heyiamkia <arrefahtazkia@gmail.com>
2025-05-29 19:19:16 +0700

    respon json

--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -1,18 +1,18 @@
 $('#search-button').on('click', function() {
 
-        $,ajax({
-            url: 'https://omdapi.com',
-            type: 'GET',
+        $.ajax({
+            url: 'https://omdbapi.com',
+            type: 'get',
             dataType: 'json',
             data: {
                 'apikey' : '8bf84afc',
-                s: $('#search-input').val()
+                's' : $('#search-input').val()
             },
             success: function(result) {
                 if (result.Response === "True") {
 
                 } else {
-                    $('#movie-list').html('<h1 class="text-center">Movie Bot Found!</h1>');
+                    $('#movie-list').html('<div class="col"><h1 class="text-center">' + result.Error + '</h1></div>');
                 }
 
 
