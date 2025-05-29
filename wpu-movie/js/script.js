a31eee6e2f9f51b99f4af4714a149e46917750d2
heyiamkia <arrefahtazkia@gmail.com>
2025-05-29 18:58:37 +0700

    get data from public API

--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -8,12 +8,9 @@ $(search-button).on('click', function() {
             's': $('#search-input').val()
         },
         success: function(result) {
-            if (result.Response === "True") {
-               
-            } else {
-                $('#movie-list').html('<h1 class="text-center">' + result.Error + '</h1>');
-            }
+            console.log(result);
         },
 
     });
 });
+
