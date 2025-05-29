b579048fec6f88c3a1d9253d4f76ab12cc940de2
heyiamkia <arrefahtazkia@gmail.com>
2025-05-29 16:05:47 +0700

    json to objek

--- json/script.js
+++ json/script.js
@@ -1,8 +1,19 @@
-let mahasiswa  ={
-    "nama": "Budi",
-    "nim": "123456789",
-    "email": "budi@gmail.com"
+// let mahasiswa  ={
+//     "nama": "Budi",
+//     "nim": "123456789",
+//     "email": "budi@gmail.com"
+// }
+
+// console.log(JSON.stringify(mahasiswa));
+
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        let mahasiswa = JSON.parse(this.responseText);
+        console.log(mahasiswa);
+    }
 }
 
-console.log(JSON.stringify(mahasiswa));
+xhr.open('GET', 'coba.json', true);
+xhr.send();
 
