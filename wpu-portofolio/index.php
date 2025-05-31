3e52b017c0bf0632530d37ce8cb05eb31774fe96
heyiamkia <arrefahtazkia@gmail.com>
2025-06-01 01:03:09 +0700

    p

--- wpu-portofolio/index.php
+++ wpu-portofolio/index.php
@@ -1,17 +1,60 @@
 <?php
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCOmHUn--16B90oW2L6FRR3A&key=AIzaSyCGH7k8h_syEjsaA_OtWVSdvw6bpjcBYVU");
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($curl);
-curl_close($curl);
 
-$result = json_decode($result, true);
+// YouTube API Calls
+$youtubeApiKey = 'AIzaSyCGH7k8h_syEjsaA_OtWVSdvw6bpjcBYVU';
+$channelId = 'UCOmHUn--16B90oW2L6FRR3A';
 
-$youtubeProfilePicture = $result['items'][0]['snippet']['thumbnails']['medium']['url'];
-$channelName= $result['items'][0]['snippet']['title'];
-$subscriber= $result['items'][0]['statistics']['subscriberCount'];
+// Get Channel Info
+$channelResult = get_CURL("https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=$channelId&key=$youtubeApiKey");
 
+// Set YouTube data with fallback values
+$youtubeProfilePicture = 'img/default-profile.jpg';
+$channelName = 'Channel Name Not Available';
+$subscriber = '0';
+
+if (isset($channelResult['items'][0])) {
+    $youtubeProfilePicture = $channelResult['items'][0]['snippet']['thumbnails']['medium']['url'] ?? $youtubeProfilePicture;
+    $channelName = $channelResult['items'][0]['snippet']['title'] ?? $channelName;
+    $subscriber = $channelResult['items'][0]['statistics']['subscriberCount'] ?? $subscriber;
+}
+
+// Get Latest Video
+$videoResult = get_CURL("https://www.googleapis.com/youtube/v3/search?key=$youtubeApiKey&channelId=$channelId&maxResults=1&order=date&part=snippet");
+$latestvideoId = 'dQw4w9WgXcQ'; 
+if (isset($videoResult['items'][0]['id']['videoId'])) {
+    $latestvideoId = $videoResult['items'][0]['id']['videoId'];
+}
+
+
+$apiToken = 'apify_api_12vNEPRTU9t6LCy6vm28wtFTvmOqFc35jYBL'; 
+$runId = '750gZ7K0BSxrqh2Af';
+$apiUrl = "https://api.apify.com/v2/actor-runs/$runId/dataset/items?format=json&clean=true&token=$apiToken";
+
+
+$profileResult = get_CURL($apiUrl);
+
+$instagramUsername = 'Instagram Not Available';
+$instagramFollowers = '0';
+
+
+if (isset($profileResult[0])) {
+    $data = $profileResult[0];
+    $instagramUsername = $data['username'] ?? $instagramUsername;
+    $instagramFollowers = $data['followersCount'] ?? $instagramFollowers;
+
+}
+
+function get_CURL($url) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($result, true);
+}
 ?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -31,7 +74,7 @@ $subscriber= $result['items'][0]['statistics']['subscriberCount'];
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#home">Sandhika Galih</a>
+        <a class="navbar-brand" href="#home">Tazkia Arrefah</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -51,17 +94,15 @@ $subscriber= $result['items'][0]['statistics']['subscriberCount'];
       </div>
     </nav>
 
-
     <div class="jumbotron" id="home">
       <div class="container">
         <div class="text-center">
-          <img src="img/profile1.png" class="rounded-circle img-thumbnail">
-          <h1 class="display-4">Sandhika Galih</h1>
-          <h3 class="lead">Lecturer | Programmer | Youtuber</h3>
+          <img src="img/picture.jpg" class="rounded-circle img-thumbnail">
+          <h1 class="display-4">Tazkia Arrefah</h1>
+          <h3 class="lead">Student | Editor | Tiktoker</h3>
         </div>
       </div>
     </div>
-
 
     <!-- About -->
     <section class="about" id="about">
@@ -95,51 +136,45 @@ $subscriber= $result['items'][0]['statistics']['subscriberCount'];
           <div class="col-md-5">
             <div class="row">
               <div class="col-md-4">
-                <img src="<?= $youtubeProfilePicture ?>" widht="200" class="rounded-circle img-thumbnail">
+                <img src="<?= $youtubeProfilePicture ?>" width="200" class="rounded-circle img-thumbnail">
               </div>
               <div class="col-md-8">
-                <h5><?= $channelName ?></h5>
-                <p><?= $subscriber ?> Subscribers</p>
+                <h5><?= htmlspecialchars($channelName) ?></h5>
+                <p><?= number_format($subscriber) ?> Subscribers</p>
+                <div class="g-ytsubscribe" data-channelid="<?= $channelId ?>" data-layout="default" data-count="default"></div>
               </div>
             </div>
             <div class="row mt-3 pb-3">
               <div class="col">
                 <div class="ratio ratio-16x9">
-                  <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+                  <iframe src="https://www.youtube.com/embed/<?= $latestvideoId ?>?rel=0" title="YouTube video" allowfullscreen></iframe>
                 </div>
               </div>
             </div>
+          </div>
+          
           <div class="col-md-5">
             <div class="row">
-              <div class="col">
-                <img src="img/profile1.png" widht="200" class="rounded-circle img-thumbnail">
+              <div class="col-md-4">
+                <img src="img\picture.jpg" width="200" class="rounded-circle img-thumbnail">
               </div>
               <div class="col-md-8">
-                <h5>@sandikhagalih</h5>
-                <p>70000 followers</p>
+                <h5>@<?= htmlspecialchars($instagramUsername) ?></h5>
+                <p><?= number_format($instagramFollowers) ?> followers</p>
               </div>
             </div>
 
             <div class="row mt-3 pb-3">
               <div class="col">
-                <div class="img-thumbnail">
-                  <img src="img/thums/1.png" alt="">
-                </div>
-                <div class="img-thumbnail">
-                  <img src="img/thums/1.png" alt="">
-                </div>
-                <div class="img-thumbnail">
-                  <img src="img/thums/1.png" alt="">
-                </div>
-                <div class="img-thumbnail">
-                  <img src="img/thums/1.png" alt="">
+                <div class="ratio ratio-16x9">
+                  <img src="img/gambar.JPG" alt="Instagram post 1" width="300px" >
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
+    </section>
 
     <!-- Portfolio -->
     <section class="portfolio" id="portfolio">
@@ -209,7 +244,6 @@ $subscriber= $result['items'][0]['statistics']['subscriberCount'];
       </div>
     </section>
 
-
     <!-- Contact -->
     <section class="contact bg-light" id="contact">
       <div class="container">
@@ -237,7 +271,6 @@ $subscriber= $result['items'][0]['statistics']['subscriberCount'];
           </div>
 
           <div class="col-lg-6">
-            
             <form>
               <div class="form-group">
                 <label for="nama">Nama</label>
@@ -259,34 +292,27 @@ $subscriber= $result['items'][0]['statistics']['subscriberCount'];
                 <button type="button" class="btn btn-primary">Send Message</button>
               </div>
             </form>
-
           </div>
         </div>
       </div>
     </section>
-
 
     <!-- footer -->
     <footer class="bg-dark text-white mt-5">
       <div class="container">
         <div class="row">
           <div class="col text-center">
-            <p>Copyright &copy; 2018.</p>
+            <p>Copyright &copy; <?= date('Y') ?></p>
           </div>
         </div>
       </div>
     </footer>
-
-
-
-
-
-
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <script src="https://apis.google.com/js/platform.js"></script>
   </body>
 </html>
